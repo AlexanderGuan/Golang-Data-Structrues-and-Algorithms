390268539483e0e526a6bbcb3cedbb3601ff472a
AlexanderGuan <alexander_guan@outlook.com>
2023-04-26 20:40:07 +0800

    add reverseRecursive

--- LinkedList/LinkedList.go
+++ LinkedList/LinkedList.go
@@ -14,6 +14,7 @@ func traverse(head *ListNode) {
 	}
 }
 
+// 递归遍历链表
 func traverseRecursive(head *ListNode) {
 	if head == nil {
 		return
@@ -21,4 +22,16 @@ func traverseRecursive(head *ListNode) {
 	// 前序位置
 	traverseRecursive(head.Next)
 	// 后序位置
+}
+
+// 递归反转整个单链表
+// 定义：输入一个单链表头结点，将该链表反转，返回新的头结点
+func reverseRecursive(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	last := reverseRecursive(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
 }
