a06d413e50a683efcf5eb1ee4e3d5db5eb35e4ee
AlexanderGuan <alexander_guan@outlook.com>
2023-04-29 15:32:04 +0800

    add reverseN

--- LinkedList/LinkedList.go
+++ LinkedList/LinkedList.go
@@ -35,3 +35,22 @@ func reverseRecursive(head *ListNode) *ListNode {
 	head.Next = nil
 	return last
 }
+
+// 递归反转链表前N个节点
+var successor *ListNode
+
+// 定义：反转以 head 为起点的 n 个节点，返回新的头结点
+func reverseN(head *ListNode, n int) *ListNode {
+	if n == 1 {
+		// 记录第 n + 1 个节点
+		successor = head.Next
+		return head
+	}
+	// 以 head.Next 为起点，需要反转前 n - 1 个节点
+	last := reverseN(head.Next, n-1)
+
+	head.Next.Next = head
+	// 让反转之后的 head 节点和后面的节点连起来
+	head.Next = successor
+	return last
+}
